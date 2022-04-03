897791c134078a9d69332518eee1b98fa8cdd9bf
andrewske <andrewskevin92@gmail.com>
2022-04-03 12:28:46 -0700

    fix url in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import { serverUrl } from './config';
 
 (async () => {
   let stripePromise;
 
   if (process.env.NODE_ENV === 'production') {
     try {
-      const { publishableKey } = await fetch('api/stripe/payment/config').then(
-        (r) => r.json()
-      );
+      const { publishableKey } = await fetch(
+        `${serverUrl}/stripe/payment/config`
+      ).then((r) => r.json());
       stripePromise = loadStripe(publishableKey);
     } catch (error) {
       console.log('Could not load stripe: ', error.message);
