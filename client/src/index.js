92577b75aa2e5506bb94b54b99d877b4cf5756f2
andrewske <andrewskevin92@gmail.com>
2022-04-03 12:17:53 -0700

    remove config from client

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-const config = require('config');
 
 (async () => {
   let stripePromise;
