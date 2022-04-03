eb30f93e54dabea76009024936ef22c084269268
andrewske <andrewskevin92@gmail.com>
2022-04-03 13:46:25 -0700

    more cors

--- server.js
+++ server.js
@@ -15,25 +15,16 @@ const app = express();
 
 connectDB();
 
-// app.use(
-//   cors({
-//     origin: ['https://www.masakaliretreat.com', 'http://localhost:3000'],
-//     credentials: true,
-//     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: ['https://www.masakaliretreat.com', 'http://localhost:3000'],
+    credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
+app.options('*', cors());
 
 // Sessions
 // app.use(
