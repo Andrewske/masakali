02096c9ef4ba1815bae85095f244927ec793b2d5
andrewske <andrewskevin92@gmail.com>
2022-04-03 12:57:20 -0700

    change fetch to axios and add headers:

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,44 @@ import App from './App';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { serverUrl } from './config';
+import axios from 'axios';
 
 (async () => {
   let stripePromise;
 
   if (process.env.NODE_ENV === 'production') {
     try {
-      const { publishableKey } = await fetch(
-        `${serverUrl}/stripe/payment/config`
-      ).then((r) => r.json());
+      const config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'GET',
+        url: `${serverUrl}/stripe/payment/config`,
+        withCredentials: true,
+      };
+      const { publishableKey } = await axios(config).then((r) => r.data);
       stripePromise = loadStripe(publishableKey);
     } catch (error) {
       console.log('Could not load stripe: ', error.message);
     }
   } else {
-    stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+    try {
+      const config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'GET',
+        url: `${serverUrl}/stripe/payment/config`,
+        withCredentials: true,
+      };
+      const { publishableKey } = await axios(config).then((r) => r.data);
+      stripePromise = loadStripe(publishableKey);
+    } catch (error) {
+      console.log('Could not load stripe: ', error.message);
+    }
+    //stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
   }
 
   ReactDOM.render(
