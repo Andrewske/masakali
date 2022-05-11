07dca3cba71aa6422797dd74c72e1154054aa86f
andrewske <andrewskevin92@gmail.com>
2022-05-11 16:41:36 -0700

    update coutnry reducer

--- client/src/reducers/country.js
+++ client/src/reducers/country.js
@@ -14,7 +14,7 @@ export default function (state = initialState, action) {
     case SET_RATES:
       return {
         ...state,
-        rates: payload,
+        rates: payload.data,
       };
     case SET_COUNTRY:
       return {
