76ba04e956f8cfb21a627b503224db4ec65e6636
andrewske <andrewskevin92@gmail.com>
2022-05-11 18:50:16 -0700

    get key from server

--- client/src/index.js
+++ client/src/index.js
@@ -9,31 +9,47 @@ import axios from 'axios';
 (async () => {
   let stripePromise;
 
-  if (process.env.NODE_ENV === 'production') {
-    try {
-      console.log(process.env.STRIPE_PUBLISHABLE_KEY);
-      stripePromise = loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
-    } catch (error) {
-      console.log('Could not load stripe: ', error.message);
-    }
-  } else {
-    try {
-      const config = {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        method: 'GET',
-        url: `${serverUrl}/stripe/payment/config`,
-        withCredentials: true,
-      };
-      const { publishableKey } = await axios(config).then((r) => r.data);
-      stripePromise = loadStripe(publishableKey);
-    } catch (error) {
-      console.log('Could not load stripe: ', error.message);
-    }
-    //stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  try {
+    const config = {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+      url: `${serverUrl}/stripe/payment/config`,
+      withCredentials: true,
+    };
+    const { publishableKey } = await axios(config).then((r) => r.data);
+    stripePromise = loadStripe(publishableKey);
+  } catch (error) {
+    console.log('Could not load stripe: ', error.message);
   }
+
+  // if (process.env.NODE_ENV === 'production') {
+  //   try {
+  //     console.log(process.env.STRIPE_PUBLISHABLE_KEY);
+  //     stripePromise = loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+  //   } catch (error) {
+  //     console.log('Could not load stripe: ', error.message);
+  //   }
+  // } else {
+  //   try {
+  //     const config = {
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-Type': 'application/json',
+  //       },
+  //       method: 'GET',
+  //       url: `${serverUrl}/stripe/payment/config`,
+  //       withCredentials: true,
+  //     };
+  //     const { publishableKey } = await axios(config).then((r) => r.data);
+  //     stripePromise = loadStripe(publishableKey);
+  //   } catch (error) {
+  //     console.log('Could not load stripe: ', error.message);
+  //   }
+  //   //stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  // }
 
   ReactDOM.render(
     <React.StrictMode>
