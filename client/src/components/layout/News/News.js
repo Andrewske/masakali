9990ae33be0d15168f46a33f8916997768b3df46
andrewske <andrewskevin92@gmail.com>
2022-09-09 12:27:30 -0700

    finish construction message modal

--- client/src/components/layout/News/News.js
+++ client/src/components/layout/News/News.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import ImageContext from '../../../utils/ImageContext';
+import { IKImage } from 'imagekitio-react';
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div className='news-container'>
+      <div className='news-main-image'>
+        <ImageContext>
+          <IKImage
+            className='template-gallery-item'
+            path='/Main/Surya/surya-front-main_yynphR5d1s.webp'
+            transformation={[{ height: '775px', width: 'auto', dpr: 'auto' }]}
+            lqip={{ active: true }}
+            loading='lazy'
+          />
+        </ImageContext>
+      </div>
+      <div className='news-content'></div>
+    </div>
+  );
 };
 
 export default News;
