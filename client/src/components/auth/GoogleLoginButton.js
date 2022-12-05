5f6fddddd68c50ee9b70df79e8b73918a8a38802
andrewske <andrewskevin92@gmail.com>
2022-12-05 14:56:52 -0800

    use Google login button img instead

--- client/src/components/auth/GoogleLoginButton.js
+++ client/src/components/auth/GoogleLoginButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { serverUrl } from '../../config';
+import googleBtn from '../../img/btn_google_signin_dark_normal_web@2x.png';
 
 const GoogleLoginButton = () => {
   const handleClick = (e) => {
@@ -7,23 +8,13 @@ const GoogleLoginButton = () => {
   };
 
   return (
-    <span
+    <img
+      src={googleBtn}
       className='google-btn'
       onClick={(e) => handleClick(e)}
       id='cart_google_login'
-    >
-      <div className='google-icon-wrapper' id='cart_google_login'>
-        <img
-          className='google-icon'
-          src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-          alt='google-icon'
-          id='cart_google_login'
-        />
-      </div>
-      <p className='btn-text' id='cart_google_login'>
-        Sign in with Google
-      </p>
-    </span>
+      alt='google_login_btn'
+    />
   );
 };
 
