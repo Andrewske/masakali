cf9f6247147d642d6059c620d055cb205f0f5986
andrewske <andrewskevin92@gmail.com>
2022-12-05 14:25:06 -0800

    add cart_google_login id to all elements in button

--- client/src/components/auth/GoogleLoginButton.js
+++ client/src/components/auth/GoogleLoginButton.js
@@ -2,20 +2,26 @@ import React from 'react';
 import { serverUrl } from '../../config';
 
 const GoogleLoginButton = () => {
-  const handleClick = () => {
+  const handleClick = (e) => {
     window.open(`${serverUrl}/auth/google?redirect=cart`, '_self');
   };
+
   return (
-    <span className='google-btn' onClick={handleClick} id='cart_google_login'>
-      <div className='google-icon-wrapper'>
+    <span
+      className='google-btn'
+      onClick={(e) => handleClick(e)}
+      id='cart_google_login'
+    >
+      <div className='google-icon-wrapper' id='cart_google_login'>
         <img
           className='google-icon'
           src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
           alt='google-icon'
+          id='cart_google_login'
         />
       </div>
-      <p className='btn-text'>
-        <b>Sign in with google</b>
+      <p className='btn-text' id='cart_google_login'>
+        Sign in with Google
       </p>
     </span>
   );
