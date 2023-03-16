eb0442f585bea879dc060df061638a281844167c
Kevin Andrews <andrewskevin92@gmail.com>
2023-03-16 14:12:13 -0700

    add other facilitators

--- client/src/components/layout/Retreats/Facilitators.js
+++ client/src/components/layout/Retreats/Facilitators.js
@@ -2,74 +2,45 @@ import React from 'react';
 import ImageContext from '../../../utils/ImageContext';
 import { IKImage } from 'imagekitio-react';
 import facebook from '../../../img/facebook.png';
+import { facilitatorData } from './facilitatorData';
 
 const Facilitators = () => {
   return (
     <div className="facilitators-container">
       <h2>Meet Your Facilitators</h2>
 
-      <div className="facilitator">
-        <div className="details">
-          <ImageContext>
-            <IKImage
-              className="facilitator-image"
-              path={
-                '/Retreats/Shanti/EMBODIMENT_YeP1KRu7Qo.webp?updatedAt=1678838432719'
-              }
-              transformation={[{ height: '150', width: '150', dpr: 'auto' }]}
-              lqip={{ active: true }}
-              loading={'lazy'}
-            />
-          </ImageContext>
-          <p>MAYA - Yoga</p>
-          <a
-            href="https://www.facebook.com/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img
-              src={facebook}
-              className="icon"
-              alt=""
-            />
-          </a>
+      {Object.entries(facilitatorData).map(([id, data]) => (
+        <div
+          className="facilitator"
+          key={id}
+        >
+          <div className="details">
+            <ImageContext>
+              <IKImage
+                className="facilitator-image"
+                path={data.imgUrl}
+                transformation={[{ height: '150', width: '150', dpr: 'auto' }]}
+                lqip={{ active: true }}
+                loading={'lazy'}
+              />
+            </ImageContext>
+            <p>{data.name}</p>
+            <p>{data.focus}</p>
+            <a
+              href={data.facebookUrl}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img
+                src={facebook}
+                className="icon"
+                alt=""
+              />
+            </a>
+          </div>
+          {data.bio}
         </div>
-        <div className="bio">
-          <p>
-            <strong>Maya</strong> is a qualified Reiki Master, Yoga
-            Practitioner, Circle Facilitator and Cacao Guardian.
-          </p>
-          <p>
-            “On my path to understand ‘who I am’ I have learnt that I am a
-            mult-dimensional being, I am a dreamer, a learner and a teacher!
-          </p>
-          <p>
-            I started my self-development journey to deeply understand my heart,
-            body, mind and soul. This road has taken me through a multitude of
-            experiences. I’ve studied the human body, the brain and how
-            important our emotions and feelings are to our overall well being.
-            In this process I have unlayered my heart, mind and body and
-            discovered my true-self!
-          </p>
-          <p>
-            Through everything that I’ve been learning I created Maya Guardian
-            Healing to serve, guide and help people to find the connection
-            between mind, body and soul by unlayering the repetitive patterns
-            and connecting with their true selves.”
-          </p>
-          <p>
-            Mixing her Yoga qualifications and experience that she has gained
-            through her self development journey, she carefully laces her yoga
-            classes with deep self-awareness, connecting the breath, mind, body,
-            and soul.
-          </p>
-          <p>
-            During the class, you will experience a deep connection with your
-            body, understand more about yourself, and release and unwind in a
-            fun and joyful way.
-          </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
---
+++ client/src/components/layout/Retreats/facilitatorData.js
@@ -0,0 +1,105 @@
+export const facilitatorData = {
+  maya: {
+    name: 'Maya',
+    focus: 'Yoga',
+    facebookUrl: 'https://www.facebook.com/',
+    imgUrl:
+      '/Retreats/Shanti/EMBODIMENT_YeP1KRu7Qo.webp?updatedAt=1678838432719',
+    bio: (
+      <div className="bio">
+        <p>
+          <strong>Maya</strong> is a qualified Reiki Master, Yoga Practitioner,
+          Circle Facilitator and Cacao Guardian.
+        </p>
+        <p>
+          “On my path to understand ‘who I am’ I have learnt that I am a
+          mult-dimensional being, I am a dreamer, a learner and a teacher!
+        </p>
+        <p>
+          I started my self-development journey to deeply understand my heart,
+          body, mind and soul. This road has taken me through a multitude of
+          experiences. I’ve studied the human body, the brain and how important
+          our emotions and feelings are to our overall well being. In this
+          process I have unlayered my heart, mind and body and discovered my
+          true-self!
+        </p>
+        <p>
+          Through everything that I’ve been learning I created Maya Guardian
+          Healing to serve, guide and help people to find the connection between
+          mind, body and soul by unlayering the repetitive patterns and
+          connecting with their true selves.”
+        </p>
+        <p>
+          Mixing her Yoga qualifications and experience that she has gained
+          through her self development journey, she carefully laces her yoga
+          classes with deep self-awareness, connecting the breath, mind, body,
+          and soul.
+        </p>
+        <p>
+          During the class, you will experience a deep connection with your
+          body, understand more about yourself, and release and unwind in a fun
+          and joyful way.
+        </p>
+      </div>
+    ),
+  },
+  jess: {
+    name: 'Jess Palmeira',
+    focus: 'Breathwork',
+    facebookUrl: 'https://www.facebook.com/',
+    imgUrl:
+      '/Retreats/Shanti/EMBODIMENT_YeP1KRu7Qo.webp?updatedAt=1678838432719',
+    bio: (
+      <div className="bio">
+        <p>
+          <strong>Jess Palmeira</strong> is a Certified Life Transition
+          Facilitator and Breathwork Facilitator. She is passionate about
+          self-exploration and healing and uses all her tool box to work on
+          herself and then on her clients, holding space for more than 500
+          individuals, couples, groups, events and transforming lives for the
+          last 5 years.
+        </p>
+        <p>
+          Her life itself is a life transition and now she brings her medicine
+          to the World to empower you to create a life where you can be fully
+          yourself, authentic, present, making conscious choices to change and
+          to transform in a container of love, freedom and inspiration.
+        </p>
+        <p>
+          Jess created her own methodology, called INSPIRE which consists of
+          bringing the medicine and power of the breath incorporated with Access
+          Consciousness tools, BodyWork and SomaticWork, Shadow work and energy
+          medicine (shamanic tools) to regulate your emotions and nervous
+          system, to reclaim your inner power back, to release, to surrender and
+          to expand your consciousness.
+        </p>
+        <p>
+          On her facilitations you will experience a deep connection with your
+          body, mind and soul, meeting all parts of yourself with ease and grace
+          to shift into the inspiring being you know you are in a self
+          discovery, gentle yet powerful way.
+        </p>
+      </div>
+    ),
+  },
+  antonella: {
+    name: 'Antomella',
+    focus: 'Ecstatic Dance',
+    facebookUrl: 'https://www.facebook.com/',
+    imgUrl:
+      '/Retreats/Shanti/EMBODIMENT_YeP1KRu7Qo.webp?updatedAt=1678838432719',
+    bio: (
+      <div className="bio">
+        <p>
+          <strong>Antonella Zidek</strong> is a professional Dancer and
+          Performer, but also an Energy - and Bodyworker facilitator. She works
+          with somatic therapy and human energetics for several years.
+        </p>
+        <p>
+          Her authentic and free expression creates a safe environment to
+          surrender to our own rhythm and vulnerability
+        </p>
+      </div>
+    ),
+  },
+};
